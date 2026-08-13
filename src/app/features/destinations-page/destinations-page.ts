cb74d7912ebb4d893c13def11d1c5e890135d49c
Arkajyoti Roy <163657852+arkajyoti-roy@users.noreply.github.com>
2026-08-13 12:25:15 +0530

    upto destination, the design has been updated

--- src/app/features/destinations-page/destinations-page.ts
+++ src/app/features/destinations-page/destinations-page.ts
@@ -13,21 +13,25 @@ import { environment } from '../../../environments/environment';
 })
 export class DestinationsPageComponent implements OnInit {
   destinations = signal<any[]>([]);
+  loading = signal<boolean>(true);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchDestinations();
   }
 
   fetchDestinations() {
+    this.loading.set(true);
     this.http.get<any[]>(`${environment.apiUrl}/tours/destinations`).subscribe({
       next: (data) => {
         const fifoData = data.sort((a, b) => a.id - b.id);
         this.destinations.set(fifoData);
+        this.loading.set(false);
       },
       error: (err) => {
         console.error('Failed to fetch destinations', err);
+        this.loading.set(false);
       }
     });
   }
